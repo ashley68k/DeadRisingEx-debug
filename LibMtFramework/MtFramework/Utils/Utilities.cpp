9b26284c7a8cbdce44697df79694727e72d9c67f
Ashley Scott <ashley68k@proton.me>
2025-07-18 12:56:00 -0400

    Rudimentary debug print unstubbing

--- LibMtFramework/MtFramework/Utils/Utilities.cpp
+++ LibMtFramework/MtFramework/Utils/Utilities.cpp
@@ -8,15 +8,21 @@
 #include <stdio.h>
 
 // Function pointers/trampolines:
+// already unstubbed, this is async printer
 void(__stdcall *DbgPrintError)(void **pPrintFunctions, const char *psFormat, __int64 arg1, __int64 arg2, __int64 arg3) = (void(__stdcall*)(void**, const char*, __int64, __int64, __int64))(__int64)GetModuleAddress((void*)0x14002F6D0);
+
+// this is basically just printf
+void(__cdecl* PrintfStub)(const char* _Format, ...) = (void(__cdecl*)(const char*, ...))(__int64)GetModuleAddress((void*)0x140650EB0);
 
 // Forward declarations:
 void __stdcall Hook_DbgPrintError(void **pPrintFunctions, const char *psFormat, __int64 arg1, __int64 arg2, __int64 arg3);
+void __cdecl Hook_PrintfStub(const char* _Format, ...);
 
 bool Utilities::InstallHooks()
 {
     // Hook debug print functions.
     DetourAttach((void**)&DbgPrintError, Hook_DbgPrintError);
+    DetourAttach((void**)&PrintfStub, Hook_PrintfStub);
 
     return true;
 }
@@ -32,4 +38,19 @@ void __stdcall Hook_DbgPrintError(void **pPrintFunctions, const char *psFormat, 
 
     // Print to the debugger.
     OutputDebugStringA(sMessage);
+}
+
+// same as above, but printf is variadic so we use va_list with vsnprintf.
+// signature should match standard printf and the printf stub seen at 0x140650EB0
+void __cdecl Hook_PrintfStub(const char* _Format, ...)
+{
+    va_list args;
+    va_start(args, _Format);
+
+    char sMessage[512] = { 0 };
+    vsnprintf(sMessage, sizeof(sMessage), _Format, args);
+
+    OutputDebugStringA(sMessage);
+
+    va_end(args);
 }
