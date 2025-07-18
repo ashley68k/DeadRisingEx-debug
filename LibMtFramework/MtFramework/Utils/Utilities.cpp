3ad24e9e5bae5041202137b001ae542f82fffa39
Ashley Scott <ashley68k@proton.me>
2025-07-18 14:26:05 -0400

    Match Ghidra analysis and DREX code-style

--- LibMtFramework/MtFramework/Utils/Utilities.cpp
+++ LibMtFramework/MtFramework/Utils/Utilities.cpp
@@ -6,23 +6,21 @@
 #include "Misc/AsmHelpers.h"
 #include "detours.h"
 #include <stdio.h>
+#include <string>
 
 // Function pointers/trampolines:
-// already unstubbed, this is async printer
 void(__stdcall *DbgPrintError)(void **pPrintFunctions, const char *psFormat, __int64 arg1, __int64 arg2, __int64 arg3) = (void(__stdcall*)(void**, const char*, __int64, __int64, __int64))(__int64)GetModuleAddress((void*)0x14002F6D0);
-
-// this is basically just printf
-void(__cdecl* PrintfStub)(const char* _Format, ...) = (void(__cdecl*)(const char*, ...))(__int64)GetModuleAddress((void*)0x140650EB0);
+void(__stdcall *DbgVariadicPrintError)(const char* psFormat,...) = (void(__stdcall*)(const char*,...))(__int64)GetModuleAddress((void*)0x140650EB0);
 
 // Forward declarations:
 void __stdcall Hook_DbgPrintError(void **pPrintFunctions, const char *psFormat, __int64 arg1, __int64 arg2, __int64 arg3);
-void __cdecl Hook_PrintfStub(const char* _Format, ...);
+void __stdcall Hook_DbgVariadicPrintError(const char* psFormat,...);
 
 bool Utilities::InstallHooks()
 {
     // Hook debug print functions.
     DetourAttach((void**)&DbgPrintError, Hook_DbgPrintError);
-    DetourAttach((void**)&PrintfStub, Hook_PrintfStub);
+    DetourAttach((void**)&DbgVariadicPrintError, Hook_DbgVariadicPrintError);
 
     return true;
 }
@@ -40,17 +38,17 @@ void __stdcall Hook_DbgPrintError(void **pPrintFunctions, const char *psFormat, 
     OutputDebugStringA(sMessage);
 }
 
-// same as above, but printf is variadic so we use va_list with vsnprintf.
-// signature should match standard printf and the printf stub seen at 0x140650EB0
-void __cdecl Hook_PrintfStub(const char* _Format, ...)
+// this is more or less the same as the last function, but it doesn't seem to have a pointer to what I believe is the caller
+void __stdcall Hook_DbgVariadicPrintError(const char* psFormat,...)
 {
     va_list args;
-    va_start(args, _Format);
+    va_start(args, psFormat);
 
     char sMessage[512] = { 0 };
-    vsnprintf(sMessage, sizeof(sMessage), _Format, args);
 
-    OutputDebugStringA(sMessage);
+    vsnprintf(sMessage, sizeof(sMessage), psFormat, args);
 
     va_end(args);
+
+    OutputDebugStringA(sMessage);
 }
